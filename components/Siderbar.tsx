86bd7838ecb33b882a71a7de1248daaebdf6e7fc
crow-editing <f.hayato0104@gmail.com>
2023-12-08 15:24:34 +0900

    added sidebar icon

--- components/Siderbar.tsx
+++ components/Siderbar.tsx
@@ -5,6 +5,7 @@ import { useMemo } from "react";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
 import Box from "./Box";
+import SidebarItem from "./SidebarItem";
 
 interface SidebarProps {
     children: React.ReactNode;
@@ -36,14 +37,30 @@ const Sidebar: React.FC<SidebarProps> = ({
                 hidden
                 md:flex
                 flex-col
-                gap-y-2
+                gap-y-2 
                 bg-black
                 h-full
                 w-[300px]
                 p-2
                 ">
                 <Box>
-                    
+                    <div className="
+                    flex
+                    flex-col
+                    gap-y-4
+                    px-5
+                    py-4
+                    ">
+                        {routes.map((item) => (
+                            <SidebarItem
+                                key={item.label}
+                                {...item}
+                            />
+                        ))}
+                    </div>
+                </Box>
+                <Box className="overflow-y-auto h-full">
+                    Song Library
                 </Box>
             </div>
         </div>
